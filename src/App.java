b6166fd9d8ad21731337b135aef705a85f8df541
Gabu <gabusingo@gmail.com>
2022-12-06 15:15:29 -0500

    hola

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * @author : Gabriel Jaya
+ * @date : 06.dic.2022
+ * @version 1.1
+ * Dterminar el are de un rectangulo
+ */
+public class App {
+    /**
+     * programa principal
+     * @param arg, no requiere
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la longitud");
+
+    }
+}
