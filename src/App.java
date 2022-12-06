8cda6c3a746a22bdb66a2af3d5be3a9545d410e2
Gabu <gabusingo@gmail.com>
2022-12-06 15:54:40 -0500

    Clases

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * @author : Gabriel Jaya
  * @date : 06.dic.2022
  * @version 1.1
- * Dterminar el are de un rectangulo
+ * Determinar el are de un rectangulo
  */
 public class App {
     /**
@@ -12,9 +12,66 @@ public class App {
      * @param arg, no requiere
      * @throws Exception
      */
-    public static void main(String[] args) throws Exception {
+    private static void calcularAreaRect() {
+        int longitud = 0, ancho = 0, areaRect = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese la longitud");
+       
+       
+       // control+D para escoger varios del mismo nombre y cambiarlos
+        System.out.print("Ingrese la longitud: ");
+        longitud = sc.nextInt();
+        System.out.print("Ingrese la ancho: ");
+        ancho = sc.nextInt();
 
+        areaRect = ancho*longitud;
+        System.out.println("El area es: "+areaRect);
+        sc.close();
     }
+    private static void showSignosAlternos() {
+        int nroTerminos = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese  el nro. terminos: ");
+        nroTerminos = sc.nextInt();
+
+        for(int i = 0; i<nroTerminos; i++){
+            //Operador terminario
+            System.out.print((i%2==0)?" + ":" - ");
+
+        }
+        sc.close();
+    }
+    private static void drawLetraC() {
+        int nroTerminos = 0;
+          Scanner sc = new Scanner(System.in);
+          System.out.print("Ingrese  el nro. terminos: ");
+          nroTerminos = sc.nextInt();
+     
+          for(int i = 0; i<nroTerminos; i++){
+              //Operador terminario
+              System.out.print((i%2==0)?" + ":" - ");
+     
+          }
+          System.out.println("");
+          for(int i = 0; i<nroTerminos; i++){
+              //Operador terminario
+              System.out.println((i%2==0)?" + ":" - ");
+     
+          }
+          for(int i = 0; i<nroTerminos; i++){
+              //Operador terminario
+              System.out.print((i%2==0)?" + ":" - ");
+     
+          }
+          sc.close();
+    }
+    public static void main(String[] args) {
+        //Declaracion de las variables e inicializadas
+               //calcularAreaRect();
+              showSignosAlternos();
+              drawLetraC();
+
+        
+        
+    }
+
 }
