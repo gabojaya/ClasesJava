156bead3bec99871e5e45ce4e4f13d62f7314657
Gabriel <gabriel.jaya@epn.edu.ec>
2022-12-20 21:55:01 -0500

    Holas

---
+++ Workshop/pkPatmicAnimal/Iguana.java
@@ -0,0 +1,12 @@
+package pkPatmicAnimal;
+
+import javax.swing.plaf.synth.SynthPasswordFieldUI;
+
+public class Iguana {
+    private String nombre = "Jauna"; 
+    
+    public void SayTuNombre()
+    {
+        System.out.println("yo soy : "+ nombre);
+    }
+}
