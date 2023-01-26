3804fb6c0dd9a815e3bb6381408549be42ee7082
Gabriel <gabriel.jaya@epn.edu.ec>
2023-01-25 20:43:15 -0500

    Hola

--- Notas/Notas2023.md
+++ Notas/Notas2023.md
@@ -35,18 +35,17 @@ throw new Exception(message: "error de conversion de datos");
 
 # Viernes 20/01/2023 Data
 
-
 ## Arquitectura N -TIER
 
 Hace segmentacion para que cada capa tenga una responsabilidad.
 
 N-TIER es la mas tradicional
 
-Sigen un orden 
+Sigen un orden
 
 - UI Componentes
 - Business Logic
-- Data Access component 
+- Data Access component
 
 ![Imagen|100](/resource/img/Data.png)
 
@@ -54,7 +53,8 @@ Sigen un orden
 
 Base de datos liviana que no necesita un entorno de ejecucion. No necesita instalacion larga.
 
-### Tipos de datos 
+### Tipos de datos
+
 ![Imagen|100](/resource/img/Datos.png)
 
 NULL = Sin informacion almacenada
@@ -72,3 +72,25 @@ Siempre se debe tener una Open Database Connectivity
 En este caso ODBC ---> sqlite
 
 Tambien se tiene que hacer referencia a la liberia que se va a implementar
+
+## Martes 24/01/2023
+
+(petShop.db) Base de datos --> tablas/vistas/procedi.almac/funciones/etc ------> SQL Transac  
+
+- Oracle  
+- SQL server/ AS400  
+- MYSQL / POSGEB  
+odbc = comunicacion/ extraer y enviar datos  
+
+1. SQLITE ver 3.0  
+2. ODBC SQLITE (.jar)  
+3. (XXXXXXXX.jar) + App  
+
+PK =  Primary Key  
+FK = Foreign Key  
+
+## Miercoles 25/01/2023 
+
+
+
+
--- resource/SQLScript/shemma.sql
+++ resource/SQLScript/shemma.sql
@@ -42,4 +42,63 @@ INSERT INTO PET (ID, NOMBRE, EDAD)  VALUES (3,"COMOTU", 3);
 
 SELECT * FROM PET;
 
+------------------------------------------------------------------
+
+CREATE TABLE MASCOTA
+(
+  idMascota INTEGER PRIMARY KEY AUTOINCREMENT,
+  FOREIGN KEY (idMT) REFERENCES MascotaTipo(idMT),
+  FOREIGN KEY (idSexo) REFERENCES SEXO(idSexo),
+  idSexo INTEGER NOT NULL REFERENCES SEXO,
+  NOMBRE VARCHAR(10) NOT NULL,
+  EDAD INTEGER  default (0),
+  ESTADO VARCHAR(1) default ('A')
+);
+
+INSERT INTO MASCOTA( idMT, idSexo, NOMBRE, EDAD, Estado) VALUES (3,2,"Mora",3,'A');
+INSERT INTO MASCOTA( idMT, idSexo, NOMBRE, EDAD, Estado) VALUES (2,1,"Oso",3,'A');
+INSERT INTO MASCOTA( idMT, idSexo, NOMBRE, EDAD, Estado) VALUES (1,3,"Lucho",4,'A');
+INSERT INTO MASCOTA( idMT, idSexo, NOMBRE, EDAD, Estado) VALUES (6,3,"Pepito",4,'A');
+
+--DROP TABLE MASCOTA;
+SELECT * FROM MASCOTA;
+
+UPDATE MASCOTA SET NOMBRE = 'osa' WHERE NOMBRE = 'Oso' AND idMT = 2;
+
+
+--DELETE FROM  MASCOTA WHERE idPet = 2;
+SELECT * FROM MASCOTA;
+
+--------------------------------------------------------------------
+
+CREATE TABLE SEXO (idSexo INTEGER PRIMARY KEY, NOMBRE VARCHAR(10) );
+
+INSERT INTO SEXO(idSexo, NOMBRE) VALUES (1, "Masculino");
+INSERT INTO SEXO(idSexo, NOMBRE) VALUES (2, "Hembra");
+INSERT INTO SEXO(idSexo, NOMBRE) VALUES (3, "S/N");
+
+--DROP TABLE SEXO; 
+SELECT * FROM SEXO;
+
+-----------------------------------------------------------------------
+
+CREATE TABLE MascotaTipo( idMT INTEGER PRIMARY KEY AUTOINCREMENT , NOMBRE VARCHAR(10) NOT NULL, Estado VARCHAR(1) NOT NULL DEFAULT ('A'));
+
+INSERT INTO MascotaTipo(NOMBRE) VALUES ("perros");
+INSERT INTO MascotaTipo(NOMBRE) VALUES ("gatos");
+INSERT INTO MascotaTipo(NOMBRE) VALUES ("tortuga");
+INSERT INTO MascotaTipo(NOMBRE) VALUES ("peces");
+INSERT INTO MascotaTipo(NOMBRE) VALUES ("cuyes");
+--INSERT INTO MascotaTipo(idMT, NOMBRE) VALUES (10, "cuyes");
+--INSERT INTO MascotaTipo(idMT, NOMBRE) VALUES (7,"cuyes");
+
+--DROP TABLE MascotaTipo;
+UPDATE MascotaTipo SET Estado = "X" WHERE idMT in (6,12);
+
+
+
+SELECT * FROM MascotaTipo;
+SELECT NOMBRE FROM MascotaTipo;
+SELECT * FROM MascotaTipo WHERE Estado = 'A';
+SELECT NOMBRE FROM MascotaTipo WHERE Estado = 'A';
 
